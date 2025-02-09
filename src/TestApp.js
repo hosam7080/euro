e5c41e28705f8a136711d9f4dd3c8e630bb00016
Abdelrhman <you@example.com>
2025-02-09 21:06:15 +0200

    login and inputfield

--- src/TestApp.js
+++ src/TestApp.js
@@ -1,10 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import Login from './pages/Login'
+import RegisterForm from './pages/Register';
 
 function TestApp() {
 	return (
 		<>
-		
+		<Login/>
+		<RegisterForm/>
 		</>
 	);
 }
